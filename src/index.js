c53a716ca00ecc2d308304337ed6236e2a171c36
axecopfire <sbraun990@gmail.com>
2019-05-18 12:26:26 -0400

    added prettier

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-var fs = require('fs'),
-    path = require('path'),
-    readline = require('readline');
+var fs = require("fs"),
+  path = require("path"),
+  readline = require("readline");
 
 // read file presented as second argument
 var filePath = path.join(__dirname, String(process.argv[2]));
 
+fs.readFile(filePath, { encoding: "utf-8" }, function(err, data) {
+  if (!err) {
+    var lineReader = readline.createInterface({
+      input: fs.createReadStream(filePath)
+    });
 
-fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-    if (!err) {
-        var lineReader = readline.createInterface({
-            input: fs.createReadStream(filePath)
-          });
-          
-          lineReader.on('line', function (line) {
-            console.log('Line from file:', line);
-          });
-    } else {
-        console.log(err);
-    }
+    lineReader.on("line", function(line) {
+      console.log("Line from file:", line);
+    });
+  } else {
+    console.log(err);
+  }
 });
-
 
 /* TODOs
 * Objects:
@@ -30,7 +28,6 @@ fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
     - process read file other arguments
 *
 * Packages to Load
-    - Webpack
     - Babel (just to enable imports)
 *
 * Track line numbers
