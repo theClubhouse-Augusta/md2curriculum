2995c522c2d2a4c2ab4b2ebc33b090adc4888029
axecopfire <sbraun990@gmail.com>
2019-05-18 14:45:48 -0400

    running babel and prettier, also starting to organize code into files

---
+++ src/app.js
@@ -0,0 +1,3 @@
+class MarkdownParser {
+  constructor() {}
+}
--- src/input/readCLI.js
+++ src/input/readCLI.js
@@ -1,9 +1,9 @@
-var fs = require("fs"),
+const fs = require("fs"),
   path = require("path"),
   readline = require("readline");
 
 // read file presented as second argument
-var filePath = path.join(__dirname, String(process.argv[2]));
+const filePath = path.join(__dirname, String(process.argv[2]));
 
 fs.readFile(filePath, { encoding: "utf-8" }, function(err, data) {
   if (!err) {
