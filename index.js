2880c84632c6d33f5892be6f3ad476e1b75f5963
axecopfire <sbraun990@gmail.com>
2019-05-18 12:14:58 -0400

    adding todo lists

--- index.js
+++ index.js
@@ -21,3 +21,23 @@ fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
 });
 
 
+/* TODOs
+* Objects:
+    - read CLI
+    - get filename
+    - process other arguments
+    - read file with arguments
+    - process read file other arguments
+*
+* Packages to Load
+    - Webpack
+    - Babel (just to enable imports)
+*
+* Track line numbers
+* Pattern match for '# Week'
+* Pattern match for '# Topics'
+* Pattern match for '# Exercise' in Week
+* log how many weeks there are
+* log how many topics there are
+* log how many exercises there are per week
+*/
