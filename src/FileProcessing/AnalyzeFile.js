1ccf2bdac2b394e8119ee1dac48c58750ff8b4c6
axecopfire <sbraun990@gmail.com>
2019-05-24 12:35:48 -0400

    working on week functionality

--- src/FileProcessing/AnalyzeFile.js
+++ src/FileProcessing/AnalyzeFile.js
@@ -1,12 +1,12 @@
 import Week from './ProcessWeek/WeekBuilder';
 export default function Analyze (data, type) {
 
+  // Build out Weeks with week count/names
   let week = new Week(data);
 
-  this.WeekNames = week.weekNames;
-  this.WeekCount = week.weekCount;
-
-  
+  // this.WeekNames = week.weekNames;
+  // this.WeekCount = week.weekCount;
+  week.addWeekContent();
 
   return this;
 }
--- src/FileProcessing/ProcessWeek/WeekBuilder.js
+++ src/FileProcessing/ProcessWeek/WeekBuilder.js
@@ -1,47 +1,77 @@
 export default class Week {
-    constructor (data) {
-        this._data = data;
-        this._weekNames = [];
-        this._weekCount = 0;
+  constructor(data) {
+    this._data = data;
+    this._weekNames = [];
+    this._weekCount = 0;
+    this._weekPattern = /(# Week.*)/g;
 
-        this._rePatterns = {
-            "week": /(Week.*)/g,
-          }
+    this._exercisesPattern = /(Exercises)/g;
+  }
 
-          this.week = {};
+  get weekCount() { 
+    addWeekCount();
+    return this._weekCount; 
+  }
+  get weekNames() { 
+    addWeekNames();
+    return this._weekNames; 
+  }
 
-          this.addWeekCount();
-          this.addWeekNames();
-        }
 
-    // set Week(week) { this.Week = week }
-    get weekCount () { return this._weekCount; }
-    get weekNames () { return this._weekNames; }
+  addWeekNames() {
+    let weekNames = [];
+    let match;
 
-    addWeekNames () {
-        let weekNames = [];
-        let match;
-        
-        while (match = this._rePatterns.week.exec(this._data)) {
-            weekNames.push(match[0]);
-        }
-        
-        return this._weekNames = weekNames;
-    } 
-        
-        
-        
-    addWeekCount () {
-        let weekCounter =  0;
-        let match;
-        
-        while (match = this._rePatterns.week.exec(this._data)) {
-            weekCounter ++;
-            // console.log("Found", match[0], "at", match.index);
-        }
-        
-        return this._weekCount = weekCounter;
+    while (match = this._weekPattern.exec(this._data)) {
+      weekNames.push(match[0]);
     }
+
+    return this._weekNames = weekNames;
+  }
+
+  addWeekCount() {
+    let weekCounter = 0;
+    let match;
+
+    while (match = this._weekPattern.exec(this._data)) {
+      weekCounter++;
+    }
+
+    return this._weekCount = weekCounter;
+  }
+
+  addWeekContent() {
+    let match;
+    let week = {};
+    let weekIndex = 0;
+    let weekContent = "";
+
+    // 1. Find the beginning of a week
+    // 2. Find the next match index
+    // 3. Add the sub string between those 2 indeces
+
+    while (match = this._weekPattern.exec(this._data)) {
+
+      console.log("Week Index:", weekIndex);
+      if(match.index === 0) {
+        continue;
+      }
+      if(weekIndex === 0 && match.index !== 0) {
+        
+      }
+
+      
+      weekContent = this._data.substring(weekIndex, match.index);
+
+      console.log("\x1b[35m", weekContent);
+      weekIndex = match.index;
+      // week.match[0] = weekContent;
+      // console.log(match[0], match.index)
+
+      // weekBuilder.push
+    }
+
+  }
 
 
 }
